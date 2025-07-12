5d8d3d3b6aed05c1b9f0fd6551f32b02a5d00359
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-07-12 20:51:01 -0300

    feat: adicionando docker

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import i18next from "./configs/i18n.config"
 import i18nextMiddleware from "i18next-http-middleware"
 import requestLogger from './middlewares/requestLogger.middleware'
 import iaRoutes from "./routes/AI.routes"
+import client from './db'
 
 const app = express()
 
@@ -13,7 +14,11 @@ app.use(express.json())
 app.use(requestLogger)
 app.use(i18nextMiddleware.handle(i18next))
 
-app.get('/', (_, res) => { res.redirect('/docs/routes.html')})
+// app.get('/', (_, res) => { res.redirect('/docs/routes.html')})
+app.get("/", async (_, res) => {
+    const result = await client.query("SELECT NOW()");
+    res.send(result.rows[0]);
+});
 app.use('/api', iaRoutes)
 app.use('/docs', express.static(path.join(__dirname, "../__docs__")))
 
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import app from "./app";
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log(`Server rodando em http://localhost:${port}`);
+    console.log(`🔵 Server rodando em http://localhost:${port}`);
 });
